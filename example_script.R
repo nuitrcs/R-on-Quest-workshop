69cc9d1b60d89389a1fe8af7fde7148cd7a9d7bf
Haley Carter <hsharoncarter@gmail.com>
2024-04-24 14:22:41 -0500

    added example_script.R

---
+++ example_script.R
@@ -0,0 +1,21 @@
+#### R on Quest Script example
+# Haley Carter
+# Research Computing and Data Services
+# Northwestern University IT
+# April 24th 2024
+
+# file paths and working directory
+prefix <- getwd()
+datafile <- "example_script.R"
+filepath <- paste(prefix, datafile, sep = "/")
+
+# load required libraries
+library(dplyr)
+library(tidyr)
+
+# read in data
+data <- read_delim("")
+
+# create and save new data frame
+
+# create and save image
