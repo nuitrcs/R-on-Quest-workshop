c8b2a3f5367bf0e3797ca4cd61bee49afdfbcc1c
Haley Carter <hsharoncarter@gmail.com>
2024-04-24 14:54:10 -0500

    added plot to example_script.R

--- example_script.R
+++ example_script.R
@@ -8,29 +8,34 @@
 library(dplyr)
 library(tidyr)
 library(readr)
+library(ggplot2)
 
 # file paths and working directory, must be in directory with data
 prefix <- getwd()
 datafile <- "shortPartitionData.csv"
 filepath <- paste(prefix, datafile, sep = "/")
+plotfile <- "shortPartitionGraph.png"
+plotpath <- paste(prefix, plotfile, sep = "/")
 
 # read in data
 data <- read_csv(filepath)
 
-# create and save new data frame
+# summarize data
 data_summary <- data %>% 
   group_by(ST) %>% 
   summarise(count = n())
 
+# print number of running and pending jobs to command line
 jobStatement <- paste("There were", data_summary[data_summary$ST == "R", 'count'],
                       "jobs running and",
                       data_summary[data_summary$ST == "PD", 'count'],
                       "jobs pending at the time of sampling.", sep = " ")
-pend
-runningJobs
+jobStatement
 
-# create and save image
-status_plot <- ggplot()
+# create plot of jobs running vs pending
+status_plot <- ggplot(data_summary, aes(ST, count))+
+  geom_bar()
 
-
+# save plot image
+ggsave(filename = plotpath, plot = status_plot)
 
