515b6177b40740992f625ec4fd9cf4b1cc1201eb
Haley Carter <hsharoncarter@gmail.com>
2024-04-24 14:48:57 -0500

    add jobs statement to example_script.R

--- example_script.R
+++ example_script.R
@@ -4,18 +4,33 @@
 # Northwestern University IT
 # April 24th 2024
 
-# file paths and working directory
-prefix <- getwd()
-datafile <- "example_script.R"
-filepath <- paste(prefix, datafile, sep = "/")
-
 # load required libraries
 library(dplyr)
 library(tidyr)
+library(readr)
+
+# file paths and working directory, must be in directory with data
+prefix <- getwd()
+datafile <- "shortPartitionData.csv"
+filepath <- paste(prefix, datafile, sep = "/")
 
 # read in data
-data <- read_delim("")
+data <- read_csv(filepath)
 
 # create and save new data frame
+data_summary <- data %>% 
+  group_by(ST) %>% 
+  summarise(count = n())
+
+jobStatement <- paste("There were", data_summary[data_summary$ST == "R", 'count'],
+                      "jobs running and",
+                      data_summary[data_summary$ST == "PD", 'count'],
+                      "jobs pending at the time of sampling.", sep = " ")
+pend
+runningJobs
 
 # create and save image
+status_plot <- ggplot()
+
+
+
