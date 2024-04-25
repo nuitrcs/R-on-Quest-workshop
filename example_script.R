cb1be5d764bd9e655347e54d457f7a218e28cc28
Haley Carter <hsharoncarter@gmail.com>
2024-04-25 08:30:26 -0500

    save summary data frame to file

--- example_script.R
+++ example_script.R
@@ -15,6 +15,8 @@ datafile <- "shortPartitionData.csv"
 filepath <- paste(prefix, datafile, sep = "/")
 plotfile <- "shortPartitionGraph.png"
 plotpath <- paste(prefix, plotfile, sep = "/")
+csvfile <- "job_counts.csv"
+csvpath <- paste(prefix, csvfile, sep = "/")
 
 # read in data
 data <- read_csv(filepath)
@@ -28,8 +30,11 @@ data_summary <- data %>%
 jobStatement <- paste("There were", data_summary[data_summary$ST == "R", 'count'],
                       "jobs running and",
                       data_summary[data_summary$ST == "PD", 'count'],
-                      "jobs pending at the time of sampling.", sep = " ")
+                      "jobs pending at the time of sampling. Counts are saved in job_counts.csv", sep = " ")
 jobStatement
+
+# save job count csv
+write.csv(data_summary, csvpath, row.names = F)
 
 # create plot of jobs running vs pending
 status_plot <- ggplot(data_summary, aes(ST, count))+
